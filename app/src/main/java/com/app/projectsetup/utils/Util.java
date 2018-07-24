1d51ea3127e5608d6af4918eb1e5887f6816bf2d
Minhaj Nadeem <minhaj.n@emblemtechnologies.com>
2018-07-24 22:15:55 +0500

    setup splash

--- app/src/main/java/com/app/projectsetup/utils/Util.java
+++ app/src/main/java/com/app/projectsetup/utils/Util.java
@@ -1,7 +1,41 @@
 package com.app.projectsetup.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
 /**
  * Created by minhaj nadeem on 22/07/2018.
  */
 public class Util {
+
+    public static void hideSystemUI(Activity context) {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = context.getWindow().getDecorView();
+        int uiOptions =
+                //View.SYSTEM_UI_FLAG_IMMERSIVE |
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        // Hide the nav bar and status bar
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+    // Shows the system bars by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private void showSystemUI(Activity context) {
+        View decorView = context.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 }
